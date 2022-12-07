b37278fa513719047c21c099c0bea563334d5ca1
Ethan Pedersen <ethanp55@gmail.com>
2022-12-07 12:28:42 -0700

    Add routing with a dashboard view and navigation buttons

--- tour-of-heroes/src/app/hero-details/hero-details.component.ts
+++ tour-of-heroes/src/app/hero-details/hero-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Hero } from '../models/hero';
+import { HeroService } from '../services/hero.service';
 
 @Component({
   selector: 'app-hero-details',
@@ -9,9 +12,14 @@ import { Hero } from '../models/hero';
 export class HeroDetailsComponent implements OnInit {
   @Input() hero?: Hero;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location, private heroService: HeroService) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -17,16 +17,9 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
-
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
